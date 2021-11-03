4b3b524accbd13a72e98009f85c8a4db986f0edc
Warren W Dixon <warrenwdixon@sbcglobal.net>
2021-11-02 22:04:31 -0500

    Nov 2 10 pm, draw box and text

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,8 +3,10 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -14,7 +16,17 @@ class LoadingButton @JvmOverloads constructor(
     private var heightSize = 0
 
     private val valueAnimator = ValueAnimator()
+    private val buttonBackgroundPaint = Paint().apply {
+        // Smooth out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        color = ContextCompat.getColor(context, R.color.buttonBackground)
+    }
 
+    private val buttonTextPaint = Paint().apply {
+        isAntiAlias = true
+        color = ContextCompat.getColor(context, R.color.white)
+        textSize = 50.0f
+    }
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
@@ -27,6 +39,12 @@ class LoadingButton @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        canvas?.drawColor(ContextCompat.getColor(context, R.color.buttonBackground))
+        canvas?.drawRect(0f, 0f, 100f, 100f, buttonBackgroundPaint )
+       // if (buttonState == ButtonState.Loading) {
+            canvas?.drawRect(0f, 0f, 100f, 100f, buttonBackgroundPaint)
+       // }
+        canvas?.drawText("Download", 200.0f, 75.0f, buttonTextPaint)
 
     }
 
