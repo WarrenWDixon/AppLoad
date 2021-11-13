ddfec52e46e826a89a5b60d46db1717f6646d258
Warren W Dixon <warrenwdixon@sbcglobal.net>
2021-11-13 14:08:16 -0600

    Nov13 2pm animate status

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,7 +1,10 @@
 package com.udacity
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.content_detail.*
@@ -18,6 +21,21 @@ class DetailActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        animateButton()
     }
 
+        fun animateButton() {
+            Log.d("WWD", "in animateButton")
+            tv_download_status.alpha = 0f
+            tv_download_status.visibility = View.VISIBLE
+            val alphaAnimator = ValueAnimator.ofFloat(0f, 1f)
+            alphaAnimator.duration = 2000
+
+            alphaAnimator.addUpdateListener {
+                val animationAlpha = it.animatedValue as Float
+                Log.d("WWD", "animationAlpha is $animationAlpha")
+                tv_download_status.alpha = animationAlpha
+            }
+            alphaAnimator.start()
+        }
 }
